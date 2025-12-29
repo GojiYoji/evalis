d9e07701c693ef7dba19db995978addac11eb90c
souldzin <souldzin@souldzin.com>
2025-12-29 11:43:25 -0600

    chore(python): bump version to 0.1.1-dev

--- python/src/evalis/constants.py
+++ python/src/evalis/constants.py
@@ -1,12 +1,10 @@
 # WARNING: This file is auto_generated.
 #
 # - To change the __version__ in this file, use the `set_version` make target.
-# - To change
-# Please consider updating and/or using the script before making changes.
-
 
 # Version of package
 __version__ = "0.1.1-dev"
+
 
 # Evalis expression version that this package supports
 EXPRESSION_VERSION = "0.1.0"
