41e5129819e97063c3fc4b47da315b399e80a04d
souldzin <souldzin@souldzin.com>
2025-12-29 00:25:45 -0600

    fixup! feat: Add version bump scripts

--- python/src/evalis/constants.py
+++ python/src/evalis/constants.py
@@ -1,8 +1,9 @@
 # WARNING: This file is auto_generated.
-# 
+#
 # - To change the __version__ in this file, use the `set_version` make target.
-# - To change 
+# - To change
 # Please consider updating and/or using the script before making changes.
+
 
 # Version of package
 __version__ = "0.1.1-dev"
