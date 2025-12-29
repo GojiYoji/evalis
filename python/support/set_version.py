01575326456936e3792c093e59a3a285801bddb8
souldzin <souldzin@souldzin.com>
2025-12-29 11:43:00 -0600

    fix: python support/set_version lint friendly

--- python/support/set_version.py
+++ python/support/set_version.py
@@ -16,16 +16,15 @@ def main(args: list[str]):
     with CONSTANTS_PATH.open(mode="w") as f:
         f.write(
             f"""# WARNING: This file is auto_generated.
-# 
+#
 # - To change the __version__ in this file, use the `set_version` make target.
-# - To change 
-# Please consider updating and/or using the script before making changes.
 
 # Version of package
 __version__ = "{version}"
 
+
 # Evalis expression version that this package supports
-EXPRESSION_VERSION = "0.1.0"
+EXPRESSION_VERSION = \"0.1.0\"
 """
         )
 
